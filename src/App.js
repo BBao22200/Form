4edd48725055f15df25adfd03cb91b3ded76cc85
bao <bao22200@gmail.com>
2023-02-06 21:52:31 +0700

    add validation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-
+import './index.css'
 function App() {
   const initFormValue = {
     name: "",
@@ -8,67 +8,107 @@ function App() {
     description: ""
   }
   const [formValue, setFormValue] = useState(initFormValue);
-  // const [listFormValue, setlistFormValue] = useState([{}]);
+  const [formError, setFormError] = useState({});
+
+  const isEmptyValue = (value) => {
+    return !value || value.trim().length < 1;
+  }
+  const validateForm = () => {
+    const error = {};
+    setFormError(error)
+    if (isEmptyValue(formValue.name)) {
+      error["name"] = "Name is required"
+    }
+    if (isEmptyValue(formValue.email)) {
+      error["email"] = "Email is required"
+    }
+    if (isEmptyValue(formValue.phone)) {
+      error["phone"] = "Phone number is required"
+    }
+    if (isEmptyValue(formValue.description)) {
+      error["description"] = "Email is required"
+    }
+    setFormError(error)
+    return Object.keys(error).length === 0;
+  }
+  console.log(formError)
+
   const handleSubmit = (event) => {
     event.preventDefault()
-    setFormValue({
-      ...formValue
-    })
-    console.log(formValue)
+    if (validateForm()) {
+      console.log("form value", formValue)
+    } else {
+      console.log("form invalid")
+    }
+    // setFormValue({
+    //   ...formValue
+    // })
+    // console.log(formValue)
   }
   const handleChange = (event) => {
-    const {value, name} = event.target
+    const { value, name } = event.target
     setFormValue({
       ...formValue, [name]: value
     })
   }
-  const validateForm = (formValue) => {
-    if (formValue.name=="") {
-      alert("Name must be filled out");
-    }
-    return
-  }
-
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>Name</label>
-        <input
-          name="name"
-          type="text"
-          onChange={handleChange}
-          value={formValue.name}
-        /></div>
-      <div>
-        <label>Email</label>
-        <input
-          name="email"
-          type="text"
-          value={formValue.email}
-          onChange={handleChange}
-        /></div>
-      <div>
-        <label>Phone Number</label>
-        <input
-          type="tel"
-          placeholder="123-456-6789"
-          pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-          value={formValue.phone}
-          name="phone"
-          onChange={handleChange}
-        /></div>
-      <div>
-        <label>Description</label>
-        <textarea
-          rows="4"
-          cols="50"
-          value={formValue.description}
-          name="description"
-          onChange={handleChange}
-        ></textarea>
-      </div>
-      <input type="submit" />
-    </form>
+    <div className="container">
+      <form onSubmit={handleSubmit} className="form-submit">
+        <div className="input-wrapper">
+          <label>Name</label>
+          <input
+            placeholder="Name"
+            name="name"
+            type="text"
+            onChange={handleChange}
+            value={formValue.name}
+          />
+          {formError.name && (
+            <div className="error-feedback">{formError.name}</div>
+          )}
+        </div>
+        <div className="input-wrapper">
+          <label>Email</label>
+          <input
+            placeholder="Email"
+            name="email"
+            type="text"
+            value={formValue.email}
+            pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
+            onChange={handleChange}
+          />
+        </div>
+        {formError.name && (
+          <div className="error-feedback">{formError.email}</div>
+        )}
+        <div className="input-wrapper">
+          <label>Phone Number</label>
+          <input
+            type="tel"
+            placeholder="123-456-6789"
+            pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+            value={formValue.phone}
+            name="phone"
+            onChange={handleChange}
+          />
+        </div>
+        {formError.name && (
+          <div className="error-feedback">{formError.phone}</div>
+        )}
+        <div className="input-wrapper">
+          <label>Description</label>
+          <textarea
+            placeholder="Description"
+            rows="4"
+            cols="50"
+            value={formValue.description}
+            name="description"
+            onChange={handleChange}
+          ></textarea>
+        </div>
+        <input type="submit" />
+      </form>
+    </div>
   );
 }
 
